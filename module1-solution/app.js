7ca7de5c9fb46e027f0ac95ca826e5d994e611b2
Martina Cho <martina.cho@gmail.com>
2018-03-16 10:53:24 -0700

    update

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function () {
+'user strict';
+
+angular.module('LunchCheck',[])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.message = "";
+  $scope.checklunch = function () {
+    if ($scope.menuitems === "") {
+      $scope.message = "Please enter data first!";
+    } else {
+      var numOfItems = splitStringToArray($scope.menuitems).length;
+      if ( numberOfItems > 3 ) {
+        $scope.message = "Enjoy!";
+      }
+      else if ( numberOfItems > 0 && numOfItems < 4) {
+        $scope.message = "Too much!";
+      }
+    }
+  };
+
+}
+  function splitStringToArray(menuItemsString) {
+    return menuItemsString.split(",");
+  }
+
+})();
